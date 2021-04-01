9b4e51a06faee82bde4632570772f24201a1584a
Zach Schneider <me@zachschneider.com>
2021-04-01 13:03:19 -0500

    moved image(s) into its own folder and created logic for placeholder if there is no cover image

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import coverPlaceholder from '../images/no-book-cover-available.jpg';
 
 const Book = ({ book }) => {
 	const { title, author_name, cover_i = [], key } = book;
+	let cover_image;
+
+	if (cover_i.length == null && cover_i !== -1) {
+		cover_image = <img src={`https://covers.openlibrary.org/b/id/${cover_i}.jpg`} alt="cover" />;
+		console.log(cover_i, cover_i.length);
+	}
+
 	return (
 		<div className="book">
 			<p className="book-cover">
-				{cover_i
-					? <img src={`http://covers.openlibrary.org/b/id/${cover_i}.jpg`} alt="cover" />
-					: <img src="/public/no-book-cover-available.jpg" alt="cover" />}
+				{cover_image ? cover_image : <img src={coverPlaceholder} alt="no cover" />}
 			</p>
 			<div className="book-content">
 				<p className="book-title">

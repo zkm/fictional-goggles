71bd1a90ee3d75d03f8e6f61f2477ede2f6e59de
Zach Schneider <me@zachschneider.com>
2025-08-20 20:02:15 -0500

    fixed build

--- src/api/OpenLibraryClient.js
+++ src/api/OpenLibraryClient.js
@@ -1,9 +1,9 @@
-import { stringify } from "query-string";
+import queryString from "query-string";
 
 export const BASE_DOMAIN = "openlibrary.org";
 
 const get = async (uri, params) => {
-  const url = `https://${BASE_DOMAIN}${uri}?${stringify(params)}`;
+  const url = `https://${BASE_DOMAIN}${uri}?${queryString.stringify(params)}`;
 
   const response = await fetch(url, {
     cache: "no-cache",

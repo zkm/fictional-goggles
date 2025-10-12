8cb1190a43c0a06f86ad3c9c3bfda6e79bc1754a
Zach Schneider <me@zachschneider.com>
2025-10-12 17:25:40 -0500

    chore(dev-deps): bump eslint-plugin-react-hooks to ^7 and jsdom to ^27; update flat ESLint config to new plugin style; run lint/tests

--- eslint.config.js
+++ eslint.config.js
@@ -8,11 +8,7 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    extends: [js.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -22,7 +18,20 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
+      // React Hooks recommended rules
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // React Refresh recommended rule for Vite
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
